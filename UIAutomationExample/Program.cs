92e8efe62ba4b515f6071af52f897a100f59ce4b
wasabina67 <wasabina67@gmail.com>
2025-04-14 01:43:05 +0900

    Update Program.cs

--- UIAutomationExample/Program.cs
+++ UIAutomationExample/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace UIAutomationExample
+﻿using System;
+using System.Threading;
+
+namespace UIAutomationExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Thread.Sleep(5000);
         }
     }
 }
