d2d57b359e3d4cf3344b52e74f75d434b59eade7
wasabina67 <wasabina67@gmail.com>
2025-04-14 01:07:05 +0900

    update

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -37,18 +37,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(121, 184);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(16, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(123, 244);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.Size = new System.Drawing.Size(21, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "PW";
             // 
             // Form1
             // 
