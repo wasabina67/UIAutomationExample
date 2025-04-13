d572b2a0bb14483487b3b5f56ef2c728a353a0b8
wasabina67 <wasabina67@gmail.com>
2025-04-14 02:37:56 +0900

    update

--- UIAutomationExample/Program.cs
+++ UIAutomationExample/Program.cs
@@ -7,6 +7,9 @@ namespace UIAutomationExample
 {
     internal class Program
     {
+        private string yourId = "yourId";
+        private string yourPw = "yourPw";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Start App");
