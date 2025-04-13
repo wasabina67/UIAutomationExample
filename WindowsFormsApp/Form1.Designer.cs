15c350e442ee3e7495491814761de3a66af5a9fb
wasabina67 <wasabina67@gmail.com>
2025-04-14 01:09:54 +0900

    update

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -35,18 +35,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(121, 184);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.Size = new System.Drawing.Size(32, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.Location = new System.Drawing.Point(123, 244);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 12);
+            this.label2.Size = new System.Drawing.Size(43, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "PW";
             // 
