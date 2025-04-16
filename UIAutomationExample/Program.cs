a31d349fffff66432621d091ffd948941b963cf6
wasabina67 <wasabina67@gmail.com>
2025-04-16 12:08:54 +0900

    Update Program.cs

--- UIAutomationExample/Program.cs
+++ UIAutomationExample/Program.cs
@@ -27,15 +27,35 @@ namespace UIAutomationExample
             AutomationElement targetWindow = AutomationElement.FromHandle(p.MainWindowHandle);
             var txtId = targetWindow.FindFirst(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.AutomationIdProperty, "txtId"));
+            if (txtId != null && txtId.TryGetCurrentPattern(ValuePattern.Pattern, out object idPatternObj))
+            {
+                ValuePattern idPattern = (ValuePattern)idPatternObj;
+                idPattern.SetValue(yourId);
+            }
 
             var txtPw = targetWindow.FindFirst(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.AutomationIdProperty, "txtPw"));
+            if (txtPw != null && txtPw.TryGetCurrentPattern(ValuePattern.Pattern, out object pwPatternObj))
+            {
+                ValuePattern pwPattern = (ValuePattern)pwPatternObj;
+                pwPattern.SetValue(yourPw);
+            }
 
             var btnIn = targetWindow.FindFirst(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.AutomationIdProperty, "btnIn"));
+            if (btnIn != null && btnIn.TryGetCurrentPattern(InvokePattern.Pattern, out object inPatternObj))
+            {
+                InvokePattern inPattern = (InvokePattern)inPatternObj;
+                inPattern.Invoke();
+            }
 
             var btnOut = targetWindow.FindFirst(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.AutomationIdProperty, "btnOut"));
+            if (btnOut != null && btnOut.TryGetCurrentPattern(InvokePattern.Pattern, out object outPatternObj))
+            {
+                InvokePattern outPattern = (InvokePattern)outPatternObj;
+                outPattern.Invoke();
+            }
 
             // txtId
             // txtPw
